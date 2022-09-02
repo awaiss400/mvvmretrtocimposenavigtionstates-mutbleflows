b3ad0b4a1a8e2045381ad0017707848239b12004
awaiss400 <testingbscs@gmail.com>
2022-09-02 20:13:22 +0500

    retrofitcomits

--- app/src/main/java/com/stone/retromvvmgenerics/DataScreen.kt
+++ app/src/main/java/com/stone/retromvvmgenerics/DataScreen.kt
@@ -1,20 +1,16 @@
 package com.stone.retromvvmgenerics
 
-import android.widget.Toast
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
-import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.stone.retromvvmgenerics.APi.Posts
 import com.stone.retromvvmgenerics.composables.Screens
-import dagger.hilt.android.qualifiers.ApplicationContext
 
 @Composable
 fun DataScren(viewModel: Myviewmodel,navController: NavController) {
@@ -29,7 +25,8 @@ Column(Modifier.fillMaxWidth(), verticalArrangement = Arrangement.Center, horizo
     TextField(value =text1 , onValueChange ={text1=it} )
     Spacer(modifier = Modifier.height(10.dp))
     TextField(value =text2 , onValueChange ={text2=it} )
-    Button(onClick = {navController.navigate(Screens.Home.route)}) {
+    Button(onClick = {
+        navController.navigate(Screens.Home.route)}) {
         Text(text = "watch data")
     }
     Button(onClick = {
